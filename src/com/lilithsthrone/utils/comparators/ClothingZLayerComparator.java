5d8a547edff6a008c9286090e654e8e738b65dfc
TheKingInYellow <dwhitney@utexas.edu>
2019-08-22 14:24:04 -0700

    Clothing upgrade button

--- src/com/lilithsthrone/utils/comparators/ClothingZLayerComparator.java
+++ src/com/lilithsthrone/utils/comparators/ClothingZLayerComparator.java
@@ -18,8 +18,12 @@ public class ClothingZLayerComparator implements Comparator<AbstractClothing> {
 			return 1;
 			
 		} else if (o2.getClothingType().getEquipSlots().get(0).getZLayer() == o1.getClothingType().getEquipSlots().get(0).getZLayer()) {
+			if (o2.getValue() > o1.getValue()) {
+				return 1;
+			} else if (o1.getValue() > o2.getValue()) {
+				return -1;
+			}
 			return 0;
-			
 		} else {
 			return -1;
 		}
